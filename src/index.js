80f5fc3977ceae35a6261955f0ce73a5004c1ada
Fernando Martin <fernando.marting@gmail.com>
2017-08-23 23:51:30 +0200

    step 15

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+
+
 
 //Create a new component. This 	component chould produce HTML
 const App = () =>{
-	return <div>Hello!</div>;
+	return (
+		<div>
+			<SearchBar/>
+		</div>);
 }
 
 
 // Take this HMTL generated and put it on the page
 
 ReactDOM.render(<App/>,document.querySelector('.container')) ;
+
